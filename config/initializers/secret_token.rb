49e6536bee94238b647030cf2a0874040f73d73f
Joshua Roesslein <jroesslein@gmail.com>
2013-12-02 14:29:25 -0800

    Create app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grapevine::Application.config.secret_key_base = 'f30397667651f2519541870b06b77f04b213345b70f666d6d335c23d3ef919cc25422453de9fed3361845a65147a9f1690c2bc731de35788b87e3ce53385d07c'
