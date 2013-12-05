738198f9831233625dd0da702432fbc8b9f7a301
Joshua Roesslein <jroesslein@gmail.com>
2013-12-05 09:09:41 -0800

    Refresh stream every 30 seconds if new keywords.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,6 +2,10 @@ class Feed
   include Mongoid::Document
   field :keywords, type: Array
 
+  def self.all_keywords
+    all.pluck(:keywords).flatten.uniq
+  end
+
   def top_tweets
     Tweet.full_text_search(keywords.join(' ')).desc(:retweet_count, :created_at).limit(10)
   end
--- lib/feed_streamer.rb
+++ lib/feed_streamer.rb
@@ -1,54 +1,33 @@
 class FeedStreamer
   def initialize
-    @status = FeedStatus.instance
     @client = TweetStream::Client.new
     @client.on_timeline_status {|s| on_status(s)}
+    @current_keywords = []
   end
 
   def start
-    schedule_restart(30)
-    load_keywords
-    if @keywords.any?
-      log_status "Working"
-      @client.filter(track: @keywords)
-    else
-      log_status "Idle"
+    if needs_restart?
+      @client.stop_stream
+      @client.filter(track: @new_keywords)
+      @current_keywords = @new_keywords
     end
-  end
 
-  def stop
-    log_status "Stopped"
-    @client.stop_stream
-  end
-
-  def schedule_restart(delay)
-    EM::Timer.new(delay) {
-      stop
-    }
+    EM::Timer.new(30) { start }
   end
 
   private
 
-  def log_status(status)
-    puts(status)
-    @status.update_attribute(:status, status)
+  def needs_restart?
+    (@new_keywords = Feed.all_keywords) != @current_keywords
   end
 
   def on_status(status)
     status = status.retweeted_status || status
-    puts "#{status.text}"
-    Tweet.create(
-      status_id: status.id,
-      text: status.text,
-      retweet_count: status.retweet_count,
-      created_at: status.created_at
-    )
-    @status.inc(tweets_consumed: 1)
-  end
-
-  def load_keywords
-    @keywords = Feed.all.pluck(:keywords).flatten.uniq
-    @status.update_attribute(:keywords_count, @keywords.count)
+    t = Tweet.find_or_create_by(status_id: status.id) do |t|
+      t.text = status.text
+      t.created_at = status.created_at
+    end
+    t.update_attribute(:retweet_count, status.retweet_count)
   end
 end
 
